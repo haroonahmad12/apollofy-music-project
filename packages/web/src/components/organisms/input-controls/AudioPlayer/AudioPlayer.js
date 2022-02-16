8ea45683326d0303319ffd01d323f11a1510b01a
Haroon Ahmad <haroon334@gmail.com>
2022-02-16 17:57:57 +0100

    First

--- packages/web/src/components/organisms/input-controls/AudioPlayer/AudioPlayer.js
+++ packages/web/src/components/organisms/input-controls/AudioPlayer/AudioPlayer.js
@@ -2,6 +2,10 @@ import React from "react";
 import Player from "react-material-music-player";
 
 const AudioPlayer = () => {
+
+  const [mode, setMode] = React.useState("system");
+  const theme = makeTheme(mode === "system" ? (isDark ? "dark" : "light") : mode);
+
   return (
     <Player
       sx={{
